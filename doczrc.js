adc144ee07fc2887abc55c78e15e08963bbe2dcb
nguyenletan <nguyenletan@gmail.com>
2020-03-24 22:30:07 +1100

    added more collocations

--- doczrc.js
+++ doczrc.js
@@ -1,21 +1,20 @@
 export default {
   title: "PTE Academic: Pearson English Language Test",
   themeConfig: {
-    colors: {
-      header: {
-        
-      },
-    },
     fonts: {
       body: '"Avenir Next", sans-serif',
       heading: 'serif',
       size: '14px'
     },
+    lineHeights: {
+      body: 1.72,
+      heading: 1.125
+    },
     fontSizes: [
       12,
       14,
       16,
-      17,
+      18,
       24,
       32,
       44,
