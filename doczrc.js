90d8bc37e997ed914a45f83450e03947e956d978
nguyenletan <nguyenletan@gmail.com>
2020-03-24 19:38:01 +1100

    Initial commit

---
+++ doczrc.js
@@ -0,0 +1,26 @@
+export default {
+  title: "PTE Academic: Pearson English Language Test",
+  themeConfig: {
+    colors: {
+      header: {
+        
+      },
+    },
+    fonts: {
+      body: '"Avenir Next", sans-serif',
+      heading: 'serif',
+      size: '14px'
+    },
+    fontSizes: [
+      12,
+      14,
+      16,
+      17,
+      24,
+      32,
+      44,
+      64,
+      96
+    ],
+  },
+}
