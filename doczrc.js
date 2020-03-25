43217d914ac6706ffc649945e0d016b915e67d17
nguyenletan <nguyenletan@gmail.com>
2020-03-26 00:14:44 +1100

    updated body font

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,18 @@
 export default {
   title: "Pearson Tests of English",
   themeConfig: {
+    colors: {
+      text: '#000',
+      background: '#fff',
+      primary: '#07c',
+      modes: {
+        dark: {
+          text: '#fff',
+          background: '#000',
+          primary: '#0cf',
+        }
+      }
+    },
     head: {
     },
     styles: {
@@ -9,7 +21,7 @@ export default {
       },
     },
     fonts: {
-      body: '"Merriweather", sans-serif',
+      body: '"Roboto Slab", sans-serif',
       heading: 'IBM Plex Sans, serif',
       size: '16px'
     },
