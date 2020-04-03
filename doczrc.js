e359931446ccdf0bd377c5550b217140995b770f
nguyenletan <nguyenletan@gmail.com>
2020-04-03 17:52:46 +1100

    added form 119 to 130 of repeat sentence section

--- doczrc.js
+++ doczrc.js
@@ -1,27 +1,27 @@
 export default {
   title: "Pearson Tests of English",
   menu: [
-    'Pearson Tests of English',
+    "Pearson Tests of English",
     {
-      name: 'Collocations',
+      name: "Collocations",
       menu: [
-        'Introduction',
-        '1. Family',
-        '2. Relationships',
-        '3. Appearance',
-        '4. Character and Behaviour',
-        '5. Feeling',
-        '6. Houses and Apartments',
-        '7. Eating',
-        '8. Movies and Books',
-        '9. Music',
-        '10. Sports',
-        '11. Health and Sickness',
-        '12. Computers',
-        '13. Study',
-        '14. Academic English',
-        '15. Work',
-        '16. Business'
+        "Introduction",
+        "1. Family",
+        "2. Relationships",
+        "3. Appearance",
+        "4. Character and Behaviour",
+        "5. Feeling",
+        "6. Houses and Apartments",
+        "7. Eating",
+        "8. Movies and Books",
+        "9. Music",
+        "10. Sports",
+        "11. Health and Sickness",
+        "12. Computers",
+        "13. Study",
+        "14. Academic English",
+        "15. Work",
+        "16. Business"
       ]
     }
   ],
@@ -38,34 +38,23 @@ export default {
     //     }
     //   }
     // },
-    head: {
-    
-    },
+    head: {},
     styles: {
       h1: {
         /*textTransform: 'uppercase'*/
-      },
+      }
     },
     fonts: {
-      body: 'serif',
-      heading: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
-      monospace: 'Menlo, monospace',
-      size: '16px'
+      body: "serif",
+      heading:
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+      monospace: "Menlo, monospace",
+      size: "16px"
     },
     lineHeights: {
       body: 1.66,
       heading: 1.125
     },
-    fontSizes: [
-      12,
-      14,
-      17,
-      20,
-      24,
-      32,
-      40,
-      64,
-      96
-    ],
-  },
-}
+    fontSizes: [12, 14, 17, 20, 24, 32, 40, 64, 96]
+  }
+};
