7532f2ed05e9f1c1a38be488564d585cfee38522
nguyenletan <nguyenletan@gmail.com>
2020-04-06 17:25:02 +1000

    Added from 151 to 162 of Repeat sentence section

--- doczrc.js
+++ doczrc.js
@@ -30,18 +30,6 @@ export default {
     }
   ],
   themeConfig: {
-    // colors: {
-    //   text: '#000',
-    //   background: '#fff',
-    //   primary: '#07c',
-    //   modes: {
-    //     dark: {
-    //       text: '#fff',
-    //       background: '#000',
-    //       primary: '#0cf',
-    //     }
-    //   }
-    // },
     head: {},
     styles: {
       h1: {
