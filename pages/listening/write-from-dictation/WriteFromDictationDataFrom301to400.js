0b61c7dd32d08970eeb59f1f527e59c2bf6f9411
Tan Nguyen Le <nguyenletan@gmail.com>
2020-05-07 15:30:47 +1000

    added to 313. Write from dicatation section

--- pages/listening/write-from-dictation/WriteFromDictationDataFrom301to400.js
+++ pages/listening/write-from-dictation/WriteFromDictationDataFrom301to400.js
@@ -1,13 +1,141 @@
 const WriteFromDictationDataFrom301To400 = [
-{
+  {
     id: 301,
-    transcript: "Journalism faces to the crisis in the light of the digital revolution.",
-    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2Fffb2e8d2-e68e-4aa9-b274-53e0dbf17964.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=GPTy4PUyIo3fnLXLyJNuKF0%2B5A8xzV7gITP4CQ7O149LVHmtunlWsDoeNKNyvcqZTsLLCoFlMIy7QFKz4FNHoFOda3Woj4MlRjhVt6I7HIjVPZqYCdqsISSE01ArotBlCFGLhvebjnjDbF%2BG72krOOsJLW3fKhukJcJODnzths%2BPhpO3F3w29R0K2YRLr6MNAYNfdgojdkXozQ%2B1TO%2BSeZFX%2BpVBH8KdkpQ5ArMmuxz7Y0fwbvRwpFDYNb0PNhcwGtsjJ0mQ8t%2BaGIT4qKvzevVNyrqDcqZNy9sz4j8T4GtZ80B60eiO%2BOf7EW3Yy%2FEhQfEBPnzjhrLFQYOG9jkj7g%3D%3D",
-    level: "really hard",
+    transcript:
+      'Teaching assistants will receive a monthly stipend for housing.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F008eb01a-e86c-463d-87fc-c04e66f5f499.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524608000&Signature=HhMOq3tfmoEJDTg1Bd1db%2BYlcxwedeJIqXI9G732BC5e7CfBwk%2FH3cC3LdS8LIkMxugDxr1GU1H6R7tQrBY%2BVfjgabdStyHi5hUY4CljT30s2rLwjE5KKxhstaH%2ByXUumyuZGdldrlVf9kvXTkPKek0xMpZwwCOYjlr%2F6%2BGf6FvIFzxLQpuT4BGpo5t3IY1bKrY9FDAsLTmq1rbn4gnFmijMmrBlySfNW43Roo7nXxDMKEb%2FYhY4NB7eeepU86XFnf7AYyF%2FIO7Cu97vgurLaAwCoTp75L3BW3F2TRRyArizDJt%2BTZ078FXGm2swut5bjqVVBq%2FoiZda%2BHDLeDlPyg%3D%3D',
+    level: 'hard',
     times: 1,
-    priority: "normal",
-    newwords: [ "journalism /dʒɜːnəlɪzəm/ = báo chí" ],
+    priority: 'normal',
+    newwords: ['stipend /ˈstaɪpend/ = trợ cấp'],
   },
-];
+  {
+    id: 302,
+    transcript:
+      'The chemistry building is located near the entrance to the campus.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F74d1ed63-68f3-404a-b480-195360f1bdc6.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=EZ3c67Zpo8HPPbdPfIdShAgclVWCi8qspp1z%2FHuqbyq2tRjDNsEChvC3mjKA94lWI9G4Ns%2FhRGbUQuAokvCgyavDsg976yLXVXs%2FrofH06JwANDx3Vx2lktlaQv1ko%2F72gOp2OMpF%2FlkF%2FGlYPlVWHzXmu%2BcDESvTob3NqNIIWUIHscQQD%2BNCfSUVL209os4OVdKAyAOUqZ30Yzgh3HVfKvXoCOWGWCdmcTNRSlklHCvKfYBa2il7PTsE0IBeL16%2BsIHJPnN%2FmuZnY9rfFNSkUrYdq%2FP45Xc8daQfKz5st12lT6EcUdaZU8ie5knqH7Z3ympQ1lD7kZwG6rc5BNnXg%3D%3D',
+    level: 'easy',
+    times: 1,
+    priority: 'normal',
+    newwords: ['stipend /ˈstaɪpend/ = trợ cấp'],
+  },
+  {
+    id: 303,
+    transcript:
+      'Trees benefit cities by soaking up rainwater that runs off roads.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F495b9f9f-e858-4646-b5c5-0d443fed6d4e.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=MbRDdUytk8jbKqjWLul1tVjsIrP04XFGGNgUfMyOYy1rI5GWigvZzDTpvLQoKmGyn10dr5iSLDIn5QZ8dJRYit7Yw7wfGIa%2BsSYv8%2BKE6gzRGM0dIfxB6FPPq681Ytyd7i8cnxi7kADbjXSLfIU%2F19Vjs0Vu6mDxRwIJwiyVr4XUqZZSXyDhlNo1qxxPN1iv%2FWlWdMvaDgewFGSA2nnV7A2U6J0i5gp4udr6k9KMyAupBsaYcTFh0PC7Z2nTSfKwDAF61RV7tvbUZSOLrBh9TE754wCyyqImKhdzUc%2Bb5%2FTl0Nc2zxF%2Fu%2FVMySTKtXXREqhLBYnkca4ITo39fYXZPg%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: ['soaking up rainwater /ˈsəʊkɪŋ/ = thấm nước mu'],
+  },
+  {
+    id: 304,
+    transcript:
+      'The famed instrumental work exhibited a more demure compositional style.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2Fe8167e58-439f-4011-b5a5-7421a158ec9c.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=dMw0lJoENdqsv70d1f7EpdT5KWZWwY2Fh3iayLVhwiwRitUERk%2BF4XcA4dwfy3wzsUXY0f1Wkj4JewFYSB%2Bo3a4m7ATQuHCGLbz4RXPM937388sypTEkrwa230G0FZyzFWmia8D41M6aqT6w3O3A1pJ%2FSgRHMEMO8FXzppWPESanBPvJvDkvTWRNEaIixmDhi1%2FDQzIpK7F9B6NW8OaMCyA4gc1IllmwR8JlLy8tiS11ErpXDAIFU%2FeUi06vAlR284a5iRN12C3PxATMv3Kvt5WADrbOnGqpSj4ZV2EnUtXcJPGMdwb7aFgvywMiNIKjnwZf3ZeKKKaiHnMpu6yITA%3D%3D',
+    level: 'very hard',
+    times: 1,
+    priority: 'normal',
+    newwords: ['famed = nổi tiếng', 'demured /dɪˈmjʊə(r)/ = trang nghiêm'],
+  },
+  {
+    id: 305,
+    transcript:
+      'You will need to purchase an academic gown for commencement.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F272e0b8c-118f-4409-b0f6-31201ea691f2.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=M0IOyNmSZtbbdaQdqwdpK9nwBArAC2YRxVlwxbVrWJnky1K17tBcQ%2F4t8HB8bkni4taZ%2F%2FyrMnaQ%2Fj3w1tAallCFrvd2N6kt06CUJ5xMM7D8zd6WUuZ3SqB0dWw6TTOm2NwBYaa4cen2tkASc8wvG%2FJSkG4SVZQp6rq4ddHFJHv4WFixhd%2B5IjPlx9bBkQIJ0OI%2F4iBxp1gP6eavV4CRFdtVnt7K8iLd4nJl6p86HCA5c2kxWK%2BbJAaETtFDclcbXberGyipxncFfGhOUC4LXN%2Fj0DTgwxQ9z%2BfF4ccDt0VdeQ8a%2FjWXSfph%2FowAlyBHeGaFhLykpjlTng1L%2B2JPNw%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: ['gown = áo dài lễ tốt nghiệp', 'commencement = lễ trao bằng'],
+  },
+  {
+    id: 306,
+    transcript:
+      'We shall be studying the economies of several developed countries.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F98efa478-4fa1-4df1-bb22-a6089d5798a4.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=YZgW%2BXslCX0BJ%2B8LiNyNPcF2dqNdU7kbecf6AwuHYDpg9ORwGVQ0YAYpHujTWD1DF4cbYAn9sJO3ButuE1%2Bm4HijDcwLuaplv7SA6S5SpaF4NgWQxD8Tge6KsPt32Ak5JgdjVuvIK2EoTBahiK89dGMJ2PASkQwHavyOg2NZQJCAhPByr9m3kLOeI3DuUEEoSsJnTxTsFc3A2KnGf11mntA4H8IeImV8Z6edp1Tr%2FigWK%2FYppX%2BZdUbItpOQpQeJI3fRGf09WovGqWB2hMsHDjzWa%2F8sZxwoLznAjDWT6OglKNIKPAnSS5C6q6UUpWu%2BUcThkrWCSO9Up%2FAKyTfVXw%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: [],
+  },
+  {
+    id: 307,
+    transcript:
+      'Native speakers are exempted from language tests in their own language.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Ffiles%2F48hoc0r8jnaj55xjxy5v0o-WFD307_2.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524608000&Signature=Vk250rDl9LW%2FR40bZXH2JzcyhX%2FHefKarYrw4T01mJjuUYAVqlwOuOKLJIAOnHzHeUwobKbMrOHyaEY0%2B4CC12MRRdiC68sMjw0KRHujDee7IOapUT1%2FydpouNNE0kPKHiURS7BfALpGq%2BX3jZRnN3Yk%2FfWHjZB1%2FqmR8j%2F8SeiImMw86VJ5FcMWVaU6GlpPTU9pDBA9sm%2BwFStOSjwvf7ANWEP4AWbUpbK74mv1MsgUK14dd%2BzTfKc59hTRysFjhbZRo%2Bza4yqlLDxNYHQTWksSmNeeXEivv%2F%2FvhHJlQBt5QqzX8hFD08qCRqIMZkpUDEHQShe7SzwOj69%2FFzZfUw%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: ["exempt /igˈzem(p)t/ = miễn trừ"],
+  },
+  {
+    id: 308,
+    transcript: 'It is absolutely fine to acknowledge all your sources.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F085858ec-f686-4b1d-8bbf-ac89c54e5905.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=jhT8OjnuYhlJ3IoChQG8N3J1PQwNCd1ZocaQPdVVXC7CzVf1TV8Tgr6t2VaK53pp15f41LDOQdiBjB20Yv6kIrwbU5bH29N9JnctZ%2Bh7VyBo5LCc%2B0cQxhR6TNK9kSZuPBQfAbq1XOJKKxqgvzZECsPlUAuDg7wR1i5Yr4iwptgfnErI88jM%2FADGJ%2FOkzj9AVAe1FOsOFIETGI6LdCQOJ1VVoCP0Y1UWUIsECu8Ps2qfhJrY728OzRLXymlVGZkMa8P7yrrn0Dk%2BiygoN4zLr5oCouHfIncM%2FG3TDFo7CwPdt0C8eG9W82c%2B1SaHuRlkwS4nS7R8fCnyo%2FKeoJImHQ%3D%3D',
+    level: 'medium',
+    times: 1,
+    priority: 'normal',
+    newwords: ["acknowledge = biết ơn, thừa nhận"],
+  },
+  {
+    id: 309,
+    transcript: 'Avoid confusing the cause and effect of these changes.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F1544c283-cef6-489d-85a2-9138f5855afe.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=LKtilI8ammfaVy%2Fgc3iZ4zCLhPlMVJq6Xde19Ay%2BQqbEl19Pk8RsEk9qjIc%2BOtC0h2Z3hU9r2fFC22vw8DnrU5NzkKphRxGq2x1dwc8VbSByhbQi2%2FK0XX65hKUcq%2Bcw%2B15BWu7Dwf9TylzoCHnf9hIEkul%2FT2660GZKeu%2FgjNPB0eS3Ha7SRgza%2B5YF8LMKS2uKCojvjCYWyPfaX4ApssiJrV7NOIednb0Fasr4bwvLCeg2y8tXV08OQKNqK7wTNmsHzx%2FgIgMqeCHBq0ejmiMPnlWAprQtzyIlmv5fzUyK1HztYVCHVzETRCBkp7sivPrPP0sJajeRw0DVWbORiQ%3D%3D',
+    level: 'medium',
+    times: 1,
+    priority: 'normal',
+    newwords: [""],
+  },
+  {
+    id: 310,
+    transcript: 'Career opportunities in this field are quite limited.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2Fd0dd5099-35dc-48c8-880a-ea48f9363f0f.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=MKhGhId0GtLp9D7pa%2FgMwUebHNl20t6SVhlxjWFITxTvW4XkH3qzQlEMmlWXAHAiQa0Ht%2F6QnxCV4NFdXJG9QGYhIi%2BFa13nI2%2ByNjHHDBxY3ZD7gJ2XG3lsjRtkGUicbGaqFChs%2FG%2BUkLPfSzimDAqWl9EJ5B51qHGTKCjvowDD3oV%2B4Dmtb1Dsob9TXd5JWRJTeYOBUtARZbV%2BRp6X6OIzudP%2Fv%2FcXjFH%2FewPfwOrZXPIfPvCDatRSS6FO3ImlRqrOpxL18HBthK2rSvZLy6kuAMg4E4hevG1El5V7kVZS2gmWy51APkcFN9j8MvrtWFOUnppLDS09HAQEsXHeGw%3D%3D',
+    level: 'medium',
+    times: 1,
+    priority: 'normal',
+    newwords: [""],
+  },
+  {
+    id: 311,
+    transcript: 'Classical mechanics is sometimes considered a branch of applied mathematics.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2Ff5f17957-caec-4d8d-bf03-86197b2066d2.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=OBD%2BnPAbE7FsG%2BkkLzBkr3cDIpJFBISN0LwWL8pn3BqNj2YFuLMLH2lXoUhur1dosQtpisAaH1OtGtzS2dQxP3GFX11x8Wu9N1AYUN4fnUNWLvCy8hIwwdAoi75kx3%2BKS1LcROnbrm4pz1Pctbw%2FiCHP1jqyfer7O9i9PP6Ykj%2B0kkNvQZiiJwT7UjHS0osH%2B599J%2B9C5Uvo0%2FUN%2FTIMLZLurnEEe1%2BDojFJhBpOkhRJC%2Ffy0T7ku5SNovfJt5lV6mGIiLyIcafO7bATQFfAiFKn37m3jiFfa9mvcD7h3nx%2BuO8k2es7P0q7CJceKhaPGrfPv8FOMxk%2FggXwu2kQ2Q%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: ['applied mathematics = toán học ứng dụng'],
+  },
+  {
+    id: 312,
+    transcript: 'Climate change is now an accepted phenomenon among reputable scientists.',
+    audio:
+      'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F5320a189-4e0a-4f68-802e-d3946b372f7a.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=MMb7tJPAJvgdbVubzMhbpMCVZgS81nY7eLICzGEBles94xdU0zASYFedhl0Iuux1uNgPpJUtMOoIHPAy9%2F%2FjFh7xg1pPbbhqYK0riT6ckSjSAYraV%2BvaJSZUT6zlhsrtP1vnxJRGVlODYiyp2LqDI4XKOwtnoB%2FCdOAqtvKJ%2FbLuqPiKF%2BLITwkhB88Clv3Oo8EsTb6WaWVw%2FoV9Sr9nh1qc0TxWJVzxObjwcNO37HUNIdBpQUYI1nKFR12QNcSqBlVgtVZdRBBlVJ7QVo8n%2FTnEheNhZbnOMzdtQ4XUs0cl2H4ELTd%2BoBY3d4i56n7RBx0%2FCn8xD0wdBEHoaFWebg%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: [],
+  },
+  {
+    id: 313,
+    transcript: 'Clinical placements in nursing prepare students for professional practice.',
+    audio:
+  'https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Fwfd%2F48450855-e714-4a60-9f7b-b5569ca25daa.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=Pm8vs%2Bof6dp99T2Ves7YsXMyCnzLwHwg821QJqXDvuPW%2BIbkWgiL8%2FmjimMFp8b9dhROgrNXQxohPPQAa9EYsMh0D7JXF09XazhhIYpbbdopi94DZOX59FP91UfJptze1hbgjxj%2BdiXnRTTzpKVWxwhE5z5NvlxlTs6i%2FioU1qxqL3C1yxbiE0YIbiiVfzROhrgTa2uZXV6BrQ%2BMbAW4mmNHqw3YbFiudpg5puG9S6idSdEXB0x3phnLiCYqsYr6hPabU1LlxosohOihpS%2FDbQsFDhyY52FovwTvNu%2FamyB2vwfYqb8u2YmK0lx%2Fe%2FddioDtVlxpUsBUwYI1kNmvVA%3D%3D',
+    level: 'hard',
+    times: 1,
+    priority: 'normal',
+    newwords: ["Clinical placement = chỗ thực tập cho ngành y"],
+  },
+]
 
 export default WriteFromDictationDataFrom301To400
