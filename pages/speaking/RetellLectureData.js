03f35444a7cd68efd187560c81b7e2466652c6f0
Tan Nguyen Le <nguyenletan@gmail.com>
2020-05-15 23:42:25 +1000

    added to 10 of Retell Lecture section

---
+++ pages/speaking/RetellLectureData.js
@@ -0,0 +1,113 @@
+const RetellLectureData = [
+  {
+    id: 1,
+    topic: 'Australian Cities',
+    transcript: [`Australia’s location is important for the world’s exports, and its international trade is also important. Since Australia has a large territory with vast uninhabited areas, all towns are scattered around. This leads to huge expenses for transportation when using trains and ferries. The government also have to pay large amounts for its telecommunications to build up the catching between these regions. The Australian people are mainly living in five cities: Melbourne, Sydney, Perth and Brisbane and Adelaide The most special one is Perth, which is one of the most isolated cities in the world .However, this does not affect its state to be one of the largest cities in Australia. Most large companies, like the two leading companies, Telstra and Qantas, they are both based in Perth.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-1.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=CXv7lTDFlQEWaLn7jHZqcIjOSWb2QO1ThZos7508vzROLqAcrfFWQPIJm0y0C5QbV8Lh5ywYITYwnkV64Kybt1HmKQmuW8sJribMht%2FqQslycEBYx6%2F7VVVzdzVGGTKCNxyXCx0XytiLFce9Nnp14nx7UwPK380IuaNypLfR8dezz%2BXZJtaLasz04AaLI%2FlnFqqtpRSDkN7z2rrBGGYQmdsaKYhTLuOyo7l5nmNLzdg9%2BrYf%2FNPNwpPdA7LBFtdomd121eth%2Fm4FZyWRocvNGrxqtqNgBdqoFLkKlhh5sWV3N0%2Fu18HP9N2XcQkMRsKRCU4TI7AM5ivoTyi%2BcDJJUA%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 2,
+    topic: 'Sigmund Freud',
+    transcript: [`For better or worse, we live in a world profoundly affected by Sigmund Freud. If I had to ask you, choose a psycho…, no, name a famous psychologist, the answer of most of you would be Freud. He's the most famous psychologist ever and he's had a profound influence on the 20th and 21st century. Some biographical information: he was born in the 1850s; he spent most of his life in Vienna, Austria. And ah … but he died in London. He escaped to London soon after retreating there at the beginning of World War Two as the Nazis began to occupy where he lived. He’s one of the most famous scholars ever, but he's not known for any single discovery. Instead he's known for the development of an encompassing theory of mind, one that he developed over the span of many decades.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-2.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=f008638MYIBQONpogJqbz1HYYyhkzd8U7itcfQMpy1MwNBg%2F8UmAMcM5yzFbNZ7hbeFSTVZNzKTxhYhaYWHrP%2BE3zh%2Fg7PndlIYwt14mzLYgTeHtFKfEEjB8n7L%2Br0BZNomUxnDqHKb5Ok99mshDaznumas5ifHhlhpjXtqDuQlboZRImsOci03qk5rkthDJ2uvEhJbtByWBRDcDuHf6PmAtSVicUeDEn7KjKrpr3%2B%2B9bFqkYsKhW%2F366AJ8T5VhYi8zUP7%2Ba9Yp%2Fbn9x0yVTWp1jL9uDkuKYveMTqCK6besABeZe1kZYpCSeJn5Sj%2BhtCnXanrhdHVN9gTAThHIwg%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 3,
+    topic: 'Climate Change',
+    transcript: [`Some adverse effects of climate changes change agricultural production. Some lands are just merely unsuitable for growing crops. There will be millions of people facing hunger in Africa in the near future. Climate change will result in less production and less food it is difficult for developing countries to deal with climate change due to their financial status and other issues. There are many people living in hunger, especially in Africa. The climate change has devastating effects on world economy. The tropical areas on earth are dry and hot and are originally not suitable for food production. The change of climate leads to extreme weather conditions such as floods and hurricanes which exacerbates the food production issues. As a result, it leads to a continuous decline in food supply annually around 10 to 17 percent and this trend is perceived to be continuing in the future by 2070. The regions suffering the most will be some African countries.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-3.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=PfDv99Ag8a0ZEvlXGdRTqGsjhXQaWspTt2sORps%2FZi9U0UDd7ltQU%2BoGsqsU1sQ9XpkFctK3ocRA%2B3O7VjsPPKTx2XatNWCHAyoULTvYm5Sh2ySbazMc0BqcpkWAxtHi8SJ52Fs49VZmyQRnfiasNZRA2xRo7ZPtpEiyuI08il%2FxvpmORNzmFXCieAVP05WSKzA5sPPaMVvp7AwK%2FtSA9wY7FqhmWnj7aVynsZIrLwRPlJOp2X0fOABVnysCUrR0%2FkUH%2Ff5Kz4Wi9FfhSiJhD4eI4bNM0CcQDuRsuj%2FZX51NvK161h6eeMX10othTl956SQtI%2BQw1rQg5UlTxWzNwA%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 4,
+    topic: 'Black Hole',
+    transcript: [`This simulation shows what you might see if you are orbiting a black hole. The light and position of background stars around the whole are distorted by its gravity and they seem to spin around. On the right, the constellation Orion appears to approach the event horizon, the boundary from which nothing can escape. Orion stars look like they become separated and get spun around. Once the hole has passed by, Orion reappears on the left and looks normal again. Users can also experiment with different scenarios. This is what you might see if you were traveling towards a black hole with rocket engines slowing your descent. Another simulation mimics free fall into a hole. In the middle, the light of the entire universe appears to be concentrated in a bright ring.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-4.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=gi8XBWt68JbDX2eKu50EMLgnz0Ju%2FL%2BPKyPsnjg6xYMdKa8zTVDN8LBUA18aM%2BcMYIuoawtcY%2F8zK8SbCYTpfqoCQfuqHNw3%2FwbPKWsMZ9AE1OY9LVHZLKITlMLP%2F118%2B11gl8oGFYRo%2FubxU7pEOf%2Fjj0anHmX8u3FZuRC7t91YXSaJjwhscjQcb4ZtWZ5oZYTZkV4tIGWrQxsWZmajPbRHyR4LCowYWZZEVapHfaE1lbp8TOdAyyWmHFfI0Kt5Zu3nt%2F0cqV%2FCozPQJNxmYhxwHI1wiznN%2FpCBAB0MFW0qKC7ffTLZN2MIL5DmI1VgxUhF2V%2FZrDDiRafXeAMbhQ%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 5,
+    topic: 'Space Shuttle',
+    transcript: [`The shuttle was designed to be a space truck; it's a multi-purpose vehicle. We've done a tremendous number of different things with it. It's the most versatile space vehicle that has ever been built. We've used it to launch satellites. We've used it to repair satellites in orbit and put them back into orbit. We've used it to capture satellites and bring them back to Earth for repair. We've outfitted it with the space lab built by our European partners and used it before the era of the space station to do scientific research. We used it as part of our partnership with the Russians, which is still continuing, first as part of the Mir space station, where we actually prolonged the useful life of Mir by several years through logistical supply visits with the shuttle. And now, of course, we're using it to build the new international space station, which is a ... a huge international partnership.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-5.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=VLVZOVbJ1UljcM0KovTgrEDOF9eu4h3mtispJ%2FJk8YZ6aXs2gjsy6JPcLHXZvcLhAmo3pPJlpCknotwHEbkEjwiGpiZA0isb%2BNtfnWozQn0uVG5PPIUf3TVqxqg52Ou3X4TR8zL%2Bci%2BQr1B5qpoAuD6fXBWbXA%2F9nlM81LkcTGqfeCcCCn%2B%2BiuOXHVQNhWGmRQMxHEfQLDeE24HvKAEjbofcKPy0f7dR11rJPa51ELdISGf3GCI2W7rWCP%2FKzkJyzCuRkJPHvmlMLl8XEX2fmecweHuZddPgj49QwtiRtIpHe0DMlQPPyP8YnWf38z5Sk8dXnXylXpZLJN5m7o8jxA%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 6,
+    topic: 'Space Shuttle',
+    transcript: [`When you think of a leader, you may think of an individual who is above all bold. But a new study of fish called sticklebacks shows that shy individuals actually prefer to follow fish that are similarly timid.`,
+    
+    `Researchers had trios of sticklebacks with known personalities play follow the leader. The fish were placed in a tank that had some plastic plants at one end and some food hidden at the other. In some of the groups, a bold fish and a shy fish acted as leaders, while another shy fish followed. And in other groups, it was a bold fish that did the following. The researchers recorded whether the follower sallied forth more frequently with the fish that was behaviorally similar or the one that was different. What they found is that shy fish were more likely to emerge from under cover when an equally wary fellow was already out there. Bold follower fish did not seem to care which leader they followed. Of course, no matter which fish a stickleback chose to stick with, the bold fish did lead more expeditions over the course of the experiment than their more retiring friends. That’s because the bold fish initiated more trips, regardless of who might be tailing them. The findings are in the journal Biology Letters.`,
+    
+    `The researchers write that “when offered a choice of leaders, sticklebacks prefer to follow individuals whose personality matches their own, but bolder individuals may, nevertheless, be able to impose their leadership, even among shy followers, simply through greater effort.” We may soon see if such tendencies also hold true in humans, when Americans decide who they’ll follow in November.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-6.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=Qxy3Jl6w9DLR2kPIm1XnOBufD8Qt3M%2F6YAOly75kEEeR4EWnJe9o0efUeU4OFVkM1s%2B8if%2FqB7VE8s880Tb2ZQFE7aZuVNFawzbKtMCTFaLGDBRMhv%2BRfS0uSRwN0aFI5lp3CzSQ4NW3QGkQznhEDe9%2BQtfzO2c2Nyl8%2BDvFnBdTScPU9xSCH%2Bft1dyrI2SYYb4VaxYOkadTDZfut6KSok2YPgpWriDfIzLLGaI0EVd9D4jTCkEQvKoMf30zv0Cu7Tj%2F9E1KxBGdlKFgTyi%2BqlXa2fxZXVfhb8mOuR51sB3TmsNVH%2Fw%2BEZFS1BK1h0JYEg3oravDDO8%2FPpFElzrPUg%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 7,
+    topic: 'Renovation of Paris',
+    transcript: [`Cities are interesting places. Some cities are carefully planned, built for a reason and reflects the needs of the people as it grows. Others are less conscientiously designed. Paris, for example, was originall founded in the 3rd century as a small village. And with every passing generation, it grew in size and importance. It grew from a medieval city to a modern city.`,
+    
+    `But the transition was not always smooth. Emperor Napoleon III had to hire someone to oversee the rebuilding of Paris. The man he chose was Georges-Eugène Haussmann. In 1853, Haussmann began the process of renovating France’s capital city. His basic instruction was to bring light and air into the central districts, improve the sanitation in living areas, and make Paris a more modern, beautiful city.`,
+    
+    `… Haussmann’s projects included the destruction of the old, medieval neighborhoods, whitening of streets, building large parks and public squares and addition of fountains and sewer lines. To add to all of this, the size of Paris had to be increased, doubled actually. And Napoleon III issued official decrees annexing nearby suburbs to make them part of the city. One of the main priorities of this massive renovation was to connect all the districts together. If we think of Paris as a house, each district was its own room, existing essentially independently of the other districts. Napoleon III wanted it to be easier to travel between the most important district and decreed a sense of this being one unified city, not a serious of independent neighborhoods. So Haussmann created large avenues that connected the districts. More than that, he made all of the avenues look roughly the same. Buildings on a major avenue were required to be roughly the same height and style, and even had to use the same cream color stone for the façade. The result was to remove any local characteristics and create a uniformed Paris. For the first time, the city had a specific look. A style that people began to associate not with a district, but with Paris itself.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-7.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=KOd7A9HZ%2B1zheRK663jvkhXRjSWIwRpLDLmZGB318ElNueasUZF0h16VGCGmUMGt%2BCBEjcS7qJ0dqMs%2FzIhNuF9lTOzARCcelNlxzFEzae7C8E%2FTC9%2BOst2ucNR293NHHj7iVzG1Ikoyx4zplsPTQT8RQhQJ6%2BwB7mCs7H3UTXydW4k4oZ80k%2Fxn1kIox0G2wHpMz6iu2y8jTFsGLoUdZd6sv8QYOc8a0fj%2Fyf3kerqKPeSfj%2ByuMGUgtsfe5erf7vJxhXmxSdYevfKUlrsQ%2BMmO7fMla3chKE9Ol3YFQXB%2FninjHEyuyyzXEu6RCsipALWMCuaH5Q%2Fd6SgXSQmJ7A%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 8,
+    topic: 'Prokaryotic and Eukaryotic Cells',
+    transcript: [`The basis of all life on Earth is the cell. All creatures on Earth are made up of cells. I’m not gonna dwell on the biology of cells and much is it not that relevant. But do want to point out a few things. First of all, there are 2 sorts of cells. As we think all the prokaryotic cell, which is fairly simple and it’s the thing that all bacteria are made of. And then we have a eukaryotic cell, much more complicated beasts on the right, which did not appear until well into the history of the biosphere on earth. And there are many single-celled eukaryotes. But there are also and importantly multicellular organisms and all of those are based on the eukaryotic cell.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-8.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=i%2BVv%2FB0ihdLt0mNVCbAGHxGjGztdlfZEukuMJLY6WxcCEq9fPUSLwbqVZixgBoYA18NNoUJ1S2uw0eAxWnvvfnVKJEYipafFpRKU%2FKfW6zqdWMKZkTUTJOeHPTA8DC%2FS9P6%2B%2FnUu57vzHvLnvwv8o2uz8iuC74m2uLq1Qwox5gRHMLr3i0Wkj4iVqUx858cYycelOQ%2FogIylzdQbDheH3jNNAG9%2BvDK5b4xiqLJdSgk3GZ%2BNsZ1OyS35HDQeyrndQAp0rHJxI3lKxwJwApKRUSZyuyzmQZr31S7QjqAMSEnEsUS6NxPlasHWNNBi%2FbpxGizhdBNuttE99lNvpDpCQg%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 9,
+    topic: 'Musical Instrument',
+    transcript: [`The Skoog is a new university assessable musical instrument. It is designed for use by children or adults with special needs or in fact be used by anyone. It's soft; it's easy to play; it's robust; and it can be customized to suit anyone’s abilities. The school helps students with special needs by allowing them to get involved in making music themselves. It’s new instrument that they can play and they can take ownership of and start creating their own sounds and music. Traditional instruments are the shape and size and made of the materials they are because of the sound that they need to make. If you want to make a sound like a plucked string you need a string and it needs to be under tension, whereas with the Skoog, because it's a mixture of software and a sensor, then the computer can handle making the sound, so we can design an object that’s designed to be touched and designed to be played with. In developing the Skoog we are actually working with kids in the schools and in the classroom. It’s really helped us make the Skoogs something that’s usable by the children themselves. They’ve informed us massively on how it needs to work and they’ve given their opinion on colors and design. And the feedback they’ve given to us has been marvellous. It's just so enriching and it's really inspiring to actually work with kids, particularly when you can provide them with an ability to start playing their own music as opposed to just taking part through listening and listening to other
+    musicians and really learning from them.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-9.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=LiKhljznBvaWKTE9Yx8T8NwvGnRqoLcphXJivIKk3qlClVzRiQjExk3G%2B%2FYcOvQmu8N6RWMkySK27zKmzKukXfyzsFlYXNhh4YaQVe2mzxbcPS5vhEEQr6kaqd9qsTDPSCUIRU3oVq0w6bVIQIL1Ba6b1CW5B9P0YC2i7bGxw%2FRQjxMW4i5I8u%2Fw6LOHQcHSjyF%2BDlbPUMofs3hRONG2mWHrIvO1iwY1vgFoChjexonoauAjJcNPtUNsmmpA9JSDYV1svHrp3f%2FxaVSzV9dc7AT1PSYBLh0gpj51HGfwBC7Zf6Ju5oBL2C9xJLoGsP5NZcsmlHW9Cd0xRn4S4pO8KQ%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  },
+  {
+    id: 10,
+    topic: 'Melatonin',
+    transcript: [`I'm just going to take on the stuff where left off. The hormone I want to talk to you about is called melatonin. And it’s synthesized in the Pineal Gland, which is very small. It is the size of a pea in your brain. Decartes called it “the seat of soul”, and it is where melatonin is made. And it has a rhythm as well. And in the sense, it is the opposite of cortisol. It peaks at night. We call it as the darkness hormone. In every species that we studied, melatonin occurs at night. And it's a hormone that prepares you for the things, that your species, does at night. So, of course, in humans we sleep, but animals, like rodents, they are awake. It’s a hormone that is related to darkness behavior.`],
+    audio: "https://storage.googleapis.com/pte-assistant.appspot.com/app%2Faudio%2Frl-10.mp3?GoogleAccessId=firebase-adminsdk-t7i9w%40pte-assistant.iam.gserviceaccount.com&Expires=2524568400&Signature=Cofdj%2FGHupv4D4Ou2eBeSHi0snGxutVop3x%2BMwqEbslIf%2B5VM4J3NQLxjnw0AzXuCiEH82GlHvD2kVaFMJPBaZMxb86TKW9cGdeztDq9db9QnKAHTNQF7sLhGhWEdjHGvQZB5wQdBmSn5PjOk7COSd5f%2BvL6%2FucCT1ubnVDMG3fK3Lu28pqyR%2BkSCIDTcdqsjXMxWaRi1KIp4hw9Ztd%2B64ilFVsE3STIDfeKEaL6CilvXv1i2Q6QEDQ4JIiZmQtXqxBzn0JRNa9zKPMs7CHEhLld0z3SDibwqmg113U0W7G%2FRZ6USeG%2B4gYcHAiZ0xmHbp%2BE%2FIAyZdqAesNJU6TMAQ%3D%3D",
+    level: "medium",
+    times: 1,
+    priority: "normal",
+    newwords: []
+  }
+];
+
+export default RetellLectureData;
